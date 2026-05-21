2c82a2af71dca5b2dabe99f6602962bb44f021cc
Tordi-portfolio <164408531+Tordi-portfolio@users.noreply.github.com>
2026-05-21 08:42:55 +0100

    Add files via upload

---
+++ booksite/urls.py
@@ -0,0 +1,24 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from books import views as book_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('books.urls')),
+
+    # authentication
+    path("accounts/login/", book_views.login_view, name="login"),
+    path("accounts/logout/", book_views.logout_view, name="logout"),
+]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
